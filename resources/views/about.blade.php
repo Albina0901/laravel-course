33de8e565f87a2cca400512ff7fea7e0c0d31351
Albina Biybolatova <bijbolatova.albina@yandex.ru>
2022-04-13 11:54:55 +0500

    task_3 | layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,10 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <script src="{{ asset('js/app.js') }}"></script>
-</head>
-<body>
-<div class="container mt-4">
-    <h1>О блоге</h1>
+@extends('layouts.app')
+
+@section('title', 'О блоге')
+
+@section('header', 'О блоге')
+
+@section('content')
     <p>Эксперименты с Laravel</p>
     <div class="about__container">
         @foreach($aTeam as $aTeamItem)
@@ -18,6 +13,4 @@
             </div>
         @endforeach
     </div>
-</div>
-</body>
-</html>
+@endsection
